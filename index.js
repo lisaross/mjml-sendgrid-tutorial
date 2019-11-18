056611aa7885ba71cc27e2b467ef732ed43d4a76
Lisa Ross <lisa@thisisbroad.com>
2019-11-18 16:50:31 -0500

    add real content

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 //Variable setup
 const userInfo = {
-    name: 'Lisa',
-    lastname: 'Ross',
     email: process.env.RECIPENT_EMAIL,
-    people: [
-        { id: 24654, name: 'Lisa Ross' },
-        { id: 24655, name: 'Lisa McMillan' },
-    ]
+    content: {
+        p1: "Nous sommes heureux de votre participation dans le programme des avantage de carrière.",
+        p2: "Bienvenue à votre première vidéo de la série sur les conversations de carrière.",
+        p3: "Au cours des prochaines semaines, vous recevrez trois vidéos - toutes conçues pour vous aider à poursuivre votre apprentissage et à continuer sur votre lancée afin de créer la carrière que vous souhaitez.",
+        h: "Vidéo n ° 1 : Prendre contact",
+        p4: "Si vous êtes comme la plupart des participants, vous avez beaucoup de momentum. Dans cette vidéo, nous vous donnons des conseils supplémentaires pour vous aider à surmonter toute résistance à la prise de contact avec les personnes les plus importantes de votre environnement."
+    }
 }
 
 
