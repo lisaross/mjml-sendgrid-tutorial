12c0f7b9c070736c45022c6cbef7c02df8e23a63
Lisa Ross <lisa@thisisbroad.com>
2019-11-19 12:08:38 -0500

    improve structure of chain

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,31 @@ const mjml = require('gulp-mjml')
 const htmlmin = require('gulp-htmlmin')
 const mjmlEngine = require('mjml')
 
+// function buildHbrs(cb) {
+//   return src('./src/hbrs/*.hbr')
+//     .pipe(mjml(mjmlEngine, { validationLevel: 'strict' }))
+//     .on('error', handleError)
+//     .pipe(dest('./html'))
+//   cb();
+// }
+
+
 function buildMjml(cb) {
-  return src('./views/*.mjml')
+  return src('./src/mjml/*.mjml')
     .pipe(mjml(mjmlEngine, { validationLevel: 'strict' }))
     .on('error', handleError)
-    .pipe(dest('./html'))
+    .pipe(dest('./src/html'))
   cb();
 }
 
 function buildHtml(cb) {
-  return src('./html/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(dest('dist'))
+  return src('./src/html/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true, minifyCSS: true }))
+    .pipe(dest('./dist'))
   cb();
 }
 
-exports.default = series(buildMjml, build);
+exports.default = series(buildMjml, buildHtml);
 
 function handleError(err) {
   console.log(err.toString());
