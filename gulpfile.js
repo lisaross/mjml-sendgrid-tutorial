d82e520da6b4b43bfdcd0a602d669ab5df103789
Lisa Ross <lisa@thisisbroad.com>
2019-11-19 11:50:39 -0500

    use gulp series with modern style

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
-const gulp = require('gulp')
-
+const { series, dest, src } = require('gulp')
 const mjml = require('gulp-mjml')
 const htmlmin = require('gulp-htmlmin')
 const mjmlEngine = require('mjml')
+
+function buildMjml(cb) {
+  return src('./views/*.mjml')
+    .pipe(mjml(mjmlEngine, { validationLevel: 'strict' }))
+    .on('error', handleError)
+    .pipe(dest('./html'))
+  cb();
+}
+
+function buildHtml(cb) {
+  return src('./html/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('dist'))
+  cb();
+}
+
+exports.default = series(buildMjml, build);
 
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
-
-gulp.task('default', function () {
-  return gulp.src('./views/*.mjml')
-    .pipe(mjml(mjmlEngine, { validationLevel: 'strict' }))
-    .on('error', handleError)
-    .pipe(gulp.dest('./html'))
-})
-
-gulp.task('minify', () => {
-  return gulp.src('./html/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('dist'))
-})
